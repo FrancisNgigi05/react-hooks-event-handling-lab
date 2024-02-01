6ab38ef85eebaa046104fc0e699e4fd79b62a4d0
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2024-02-01 21:01:33 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
+import React from "react";
+
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function focus() {
+        console.log('Good!')
+    }
+
+    function noFocus() {
+        console.log('Hey! Eyes on me!');
+    }
+
+    return (
+        <button onFocus={ focus } onBlur={ noFocus }>
+            Eyes on me
+        </button>
+    );
+}
+
+export default EyesOnMe;
